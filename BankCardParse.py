ad856ee9d24d1b8005d13ad242aad6513368e422
logicalsingular <logicalsingular@gmail.com>
2021-02-04 17:17:35 +0200

    [add] support parse with spaces between numbers

--- BankCardParse.py
+++ BankCardParse.py
@@ -1,16 +1,10 @@
-# uncleared_text = input("Please, type Bank Card ")
-text_1 = "1234 1234 1234 1234"
-text_2 = "Alex 12/12 1234123412341234"
-text_3 = "Alex 12/12 1234 1234 1234 1234"
-
 def parser(text):
-    for n in range(len(text)):
-        if text[n:n+16:1].isdigit():
-            return text[n:n+16:1]       
-        elif text[n:n+4:1].isdigit() and text[n+5:n+5+4:1].isdigit() and text[n+10:n+10+4:1].isdigit() and text[n+15:n+15+4:1].isdigit():
-            return text[n:n+4:1]+text[n+5:n+5+4:1]+text[n+10:n+10+4:1]+text[n+15:n+15+4:1]
-
-
-print(parser(text_1))
-print(parser(text_2))
-print(parser(text_3))
+    for n in range(len(text)-15):
+        if text[n:n+16].isdigit():
+            return text[n:n+16]     
+        elif   text[n:n+4].isdigit() and text[n+5:n+5+4].isdigit() and text[n+10:n+10+4].isdigit() and text[n+15:n+15+4].isdigit():
+            if len(text[n:n+4])==4 and len(text[n+5:n+5+4])==4 and len(text[n+10:n+10+4])==4 and len(text[n+15:n+15+4])==4:
+                spaceless_text = text[n:n+19].replace(" ","")
+                if len(spaceless_text)==16:
+                    return spaceless_text
+    return False
